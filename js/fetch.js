6ffe5e45c4b802c1a402743b5becba88599c3d71
Romain Morel <morelromain86@gmail.com>
2021-01-26 16:41:25 +0100

    change fetch system for banner

--- js/fetch.js
+++ js/fetch.js
@@ -1,5 +1,5 @@
 // variable declaration
-var url_top = "http://localhost:8000/api/v1/titles/?sort_by=-imdb_score";
+
 var url_best = "http://localhost:8000/api/v1/titles/?sort_by=-imdb_score";
 var url_best2 = "http://localhost:8000/api/v1/titles/?page=2&sort_by=-imdb_score";
 var url_adventure = "http://localhost:8000/api/v1/titles/?genre=Adventure&sort_by=-imdb_score";
@@ -8,7 +8,6 @@ var url_comedy = "http://localhost:8000/api/v1/titles/?genre=Comedy&sort_by=-imd
 var url_comedy2 = "http://localhost:8000/api/v1/titles/?genre=Comedy&page=2&sort_by=-imdb_score";
 var url_horror = "http://localhost:8000/api/v1/titles/?genre=Horror&sort_by=-imdb_score";
 var url_horror2 = "http://localhost:8000/api/v1/titles/?genre=Horror&page=2&sort_by=-imdb_score";
-var best_url = "http://localhost:8000/api/v1/titles/1508669";
 
 var list_best = [
   document.getElementById("best_1"), document.getElementById("best_2"),
@@ -16,28 +15,24 @@ var list_best = [
   document.getElementById("best_5"), document.getElementById("best_6"),
   document.getElementById("best_7")
 ];
-
 var list_adventure = [
   document.getElementById("advent_1"), document.getElementById("advent_2"),
   document.getElementById("advent_3"), document.getElementById("advent_4"),
   document.getElementById("advent_5"), document.getElementById("advent_6"),
   document.getElementById("advent_7")
 ];
-
 var list_comedy = [
   document.getElementById("comedy_1"), document.getElementById("comedy_2"),
   document.getElementById("comedy_3"), document.getElementById("comedy_4"),
   document.getElementById("comedy_5"), document.getElementById("comedy_6"),
   document.getElementById("comedy_7")
 ];
-
 var list_horror = [
   document.getElementById("horror_1"), document.getElementById("horror_2"),
   document.getElementById("horror_3"), document.getElementById("horror_4"),
   document.getElementById("horror_5"), document.getElementById("horror_6"),
   document.getElementById("horror_7")
 ];
-
 var best_img = document.getElementById("best_img");
 var best_title = document.getElementById("best_title");
 var best_desc = document.getElementById("best_desc");
@@ -68,22 +63,27 @@ function fetch_cat(url, url2, list) {
   });
 }
 
+// function fetch for banner
 function fetch_top(url) {
   fetch(url)
   .then((resp) => resp.json())
   .then(function(data) {
-    best_img.src = data.image_url;
-    best_title.innerHTML = data.title;
-    best_desc.innerHTML = data.description;
+    fetch(data.results[0].url)
+    .then((resp) => resp.json())
+    .then(function(data) {
+      best_img.src = data.image_url;
+      best_title.innerHTML = data.title;
+      best_desc.innerHTML = data.description;
+      })
+    .catch(function(error) {
+      console.log(error);
+      });
     })
-  .catch(function(error) {
-    console.log(error);
-  });
 }
 
-// function fetch
+// fetch per category
+fetch_top(url_best);
 fetch_cat(url_best, url_best2, list_best);
 fetch_cat(url_adventure, url_adventure2, list_adventure);
 fetch_cat(url_comedy, url_comedy2, list_comedy);
 fetch_cat(url_horror, url_horror2, list_horror);
-fetch_top(best_url);
